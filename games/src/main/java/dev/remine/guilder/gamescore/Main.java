6510eeb727b429bf0094971b2abe764aa92760c8
RemineHD <reminegames@gmail.com>
2023-04-18 11:39:43 -0700

    hub plugin

---
+++ games/src/main/java/dev/remine/guilder/gamescore/Main.java
@@ -0,0 +1,7 @@
+package dev.remine.guilder.gamescore;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ plugins/hub/src/main/java/dev/remine/guilder/plugins/hub/GuilderHub.java
@@ -0,0 +1,17 @@
+package dev.remine.guilder.plugins.hub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GuilderHub extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
