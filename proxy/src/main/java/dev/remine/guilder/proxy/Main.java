6f255b5686f88b9ab1ee6233d79946b709a30a98
RemineHD <reminegames@gmail.com>
2023-04-18 12:46:39 -0700

    more setup + gRPC

--- proxy/src/main/java/dev/remine/guilder/proxy/Main.java
+++
@@ -1,7 +0,0 @@
-package dev.remine.guilder.proxy;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ proxy/src/main/java/dev/remine/guilder/proxy/ProxyPlugin.java
@@ -0,0 +1,13 @@
+package dev.remine.guilder.proxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ProxyPlugin extends Plugin {
+
+    @Override
+    public void onEnable()
+    {
+        getLogger().info("Plugin Loaded.");
+    }
+
+}
